844ac24612899b4d8e717d1c46bf080d4983ea82
NatashaSetiadi <natset456@gmail.com>
2021-10-23 16:39:28 -0400

    implemented pdf and print option for calendar

--- simply-weekly/src/components/Calendar.js
+++ simply-weekly/src/components/Calendar.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./Calendar.css";
+
 class Calendar extends Component {
   state = {};
     handleClick(){
         console.log("hi");
-        var xhr = new XMLHttpRequest()
-        xhr.open("POST", "http://127.0.0.1:5000/api/update/add",true)
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(JSON.stringify({"name":"JYZ","data":{"title":"find something",startTime:"2021-10-23T12:23:15","duration":500,"notes":"asfjhkjh"}}))
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", "http://127.0.0.1:5000/api/parse?name={CalName}&text={calText}", false);
+        xmlHttp.send()
   }
   render() {
     return (
