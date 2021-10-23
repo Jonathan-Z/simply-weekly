f7bb9aa618ffa470568bd728ccaedd973b72c278
NatashaSetiadi <natset456@gmail.com>
2021-10-23 01:35:01 -0400

    redesigned site

--- simply-weekly/src/components/Calendar.js
+++ simply-weekly/src/components/Calendar.js
@@ -7,54 +7,54 @@ class Calendar extends Component {
       <div className="calendar-container">
         <div id="calendar">
             <div id="times">
-                <span className="time">
+                <span className="time time1">
                     <span className="label">7:30am</span>
                 </span>
-                <span className="time">
+                <span className="time time2">
                     <span className="label">8:00am</span>
                 </span>
-                <span className="time">
+                <span className="time time1">
                     <span className="label">8:30am</span>
                 </span>
-                <span className="time">
+                <span className="time time2">
                     <span className="label">9:00am</span>
                 </span>
-                <span className="time">
+                <span className="time time1">
                     <span className="label">9:30am</span>
                 </span>
-                <span className="time">
+                <span className="time time2">
                     <span class="label">10:00am</span>
                 </span>
-                <span className="time">
+                <span className="time time1">
                     <span className="label">10:30am</span>
                 </span>
-                <span className="time">
+                <span className="time time2">
                     <span className="label">11:00am</span>
                 </span>
-                <span className="time">
+                <span className="time time1">
                     <span className="label">11:30am</span>
                 </span>
             </div>
             <div id="days">
-                <div className="day">
+                <div className="day day1">
                     <span className="label">Monday</span>
                 </div>
-                <div className="day">
+                <div className="day day2">
                     <span className="label">Tuesday</span>
                 </div>
-                <div className="day">
+                <div className="day day1">
                     <span className="label">Wednesday</span>
                 </div>
-                <div className="day">
+                <div className="day day2">
                     <span className="label">Thursday</span>
                 </div>
-                <div className="day">
+                <div className="day day1">
                     <span className="label">Friday</span>
                 </div>
-                <div className="day">
+                <div className="day day2">
                     <span className="label">Saturday</span>
                 </div>
-                <div className="day">
+                <div className="day day1">
                     <span className="label">Sunday</span>
                 </div>
             </div>
--- simply-weekly/src/components/Header.js
+++ simply-weekly/src/components/Header.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
-import './Header.css';
+// import $ from 'jquery'; 
+import "./Header.css";
 class Header extends Component {
-    state = {}
-    render(){
-        return (
-            <div className="container">
-            <link href="Header.css" rel="stylesheet"></link>
-            <h1 className="header-text">Simply Weekly</h1>
-            </div>
-        )
-    }
+  state = {};
+  render() {
+    return (
+      <div className="container">
+        {/* <div id="preloader" class="loaded">
+          <div id="status"></div>
+        </div> */}
+        <h1 className="header-text">
+          <span className="simply">Simply </span>
+          <span className="weekly">Weekly</span>
+        </h1>
+      </div>
+    );
+  }
 }
 
+// $(window).on('load', function(){
+
+// $('#status').fadeOut();
+// $('#preloader').delay(350).fadeOut('slow');
+// $('#body').delay(350).css({'overflow': 'visible'});
+
+// })
 export default Header;
